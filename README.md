99945fe543c162b2abd1372c2e91ab5cbb7712f2
yGifoom <giovisano@gmail.com>
2024-08-21 00:15:42 +0200

    updated tasks

--- README.md
+++ README.md
@@ -18,6 +18,7 @@ QUANTITATIVE DEF:
 - def 1, for a party: given the total number of preferences for a party, if a candidate has a low proportion of preferences party-wise q% (E.g. q=0.5),
 but on the total of preferences for the same party in a comune is > p% (E.g. p=50)then it is a filler candidate for a party.
 - def 2, in general: a candidate which gets q% (E.g. q=70) of their preferences from comuni in which the same candidate gets p% (E.g. p=70) of the votes, is a filler candidate
+this def 2 is flawed
 )
 
 ## Steps
@@ -27,13 +28,14 @@ but on the total of preferences for the same party in a comune is > p% (E.g. p=5
 [x] examine the dataset and clean it (17/08)
 
 ### division of comuni by population __GIOVANNI__ (19/08)
-[] how the preferences are spread out in a cumune by population 
-[] how the preferences are spread out in a cumune by population
+[X] how the preferences are spread out in a cumune by population 
+[X] how the preferences are spread out in a cumune by population 
+[] group by circoscrizione and highlight party __GIOVANNI__(21/08)
 
 ### find filler candidates
-[][] set percentages for filler candidates classification __ALESSANDRO CON DEF1,  LORENZO CON DEF2__ (19/08)
-[][] consider big, small city effects 
-[][] Does their birthplace have any correlation with origin of votes?
+[] set percentages for filler candidates classification __ALESSANDRO CON DEF1__(21/08)
+[] consider big, small city effects 
+[] Does their birthplace have any correlation with origin of votes? __LORENZO__(21/08)
 
 ### Quantify the effect of filler candidates
 [] scope of the party
@@ -44,7 +46,12 @@ but on the total of preferences for the same party in a comune is > p% (E.g. p=5
 [] Does their birthplace have any correlation with origin of votes?
 [] how candidates get elected in their party in relation with the votes they got 
 (where they voted directly or where they pushed up by their party?) 
-(how are votes distributed in a party?)
+[x] how are votes distributed in a party? __LORENZO__
+[] how actually elected canddates distribute per party __LORENZO__ (21/08 PRIORITA)
+
+### representance, spread and polarization
+[] how representative are the elected candidates
+[] how polarized were the elections
 
 ## Conclusions
 
@@ -54,3 +61,6 @@ but on the total of preferences for the same party in a comune is > p% (E.g. p=5
 - majority of people did not express preferenze
 - for visualization I random sampled from the population weighting on the population of the settlement,
 correlation was kept at the same values roughly + some variation
+
+- there seems to be no correlation between area/population/density and homogeneousness of votes
+perhaps there is too much noise, clustering by parties might make this better
--- analysis.py
+++ analysis.py
@@ -9,17 +9,20 @@ from shapely.geometry import Polygon, Point
 # how many votes does the first party in every municipio get?
 with open("data/europee_clean.csv", "r") as f:
     erp = pd.read_csv(f)
+with open("data/istat_clean.csv", "r") as f:
+    cmn = pd.read_csv(f)
 
 
 max_votes_per_comune = erp.groupby(["COMUNE"])["VOTI_LISTA"].transform(max) == erp["VOTI_LISTA"]
 df = erp[max_votes_per_comune].copy()
 df["perc_voto"] = df["VOTI_LISTA"] / df["VOTANTI"]
 print(df.columns)
+
 first_past_post = df[["COMUNE", "perc_voto", "ELETTORI"]].copy()
 print(first_past_post.head)
 
 first_sample = first_past_post.sample(frac=0.2, weights=first_past_post["ELETTORI"]/sum(first_past_post["ELETTORI"]))
-ax = first_sample.plot(x="ELETTORI", y="perc_voto", kind="scatter", logx=True)
+ax = first_past_post.plot(x="ELETTORI", y="perc_voto", kind="scatter", logx=True)
 ax.figure.savefig("plots/first_past_post.png")
 
 # correlation
@@ -27,24 +30,36 @@ corr = first_sample[["ELETTORI", "perc_voto"]].corr(method="pearson")
 print(corr)
 
 
-# best two parties?
-n  = 2
-'''n_past_post = first_past_post
-for _ in range(n-1):
-    more_parties = (df[~n_past_post.isin(df[["COMUNE", "perc_voto", "ELETTORI"]])]).groupby(["COMUNE"])["VOTI_LISTA"].transform(max) \
-    == df[~n_past_post.isin(df[["COMUNE", "perc_voto", "ELETTORI"]])]
+# best n parties?
+m = 10
+for n in range(1, m):
+    # Sort the dataframe by "comune" and "voti" in descending order
+    df_grouped = erp.sort_values(by=['COMUNE', 'VOTI_LISTA'], ascending=[True, False])
+
+    # Select the top 2 parties for each comune
+    df_top2 = df_grouped.groupby(['COMUNE', "PROVINCIA"]).head(n)
+
+    # Sum the votes of the top 2 parties for each comune
+    df_result = df_top2.groupby(['COMUNE', "PROVINCIA"]).agg({'VOTI_LISTA': 'sum', 'VOTANTI': 'first'}).reset_index()
+    df_result["perc_voto"] = df_result["VOTI_LISTA"] / df_result["VOTANTI"]
+    print(df_result)
+
+    df_final = pd.merge(df_result, cmn[["COMUNE", "SHAPE_AREA"]], on='COMUNE', how='left')
     
-    n_past_post = n_past_post.aggregate(more_parties)'''
+    res = df_final[df_final["SHAPE_AREA"] > 1]
+    
+    ax = res.plot(x="VOTANTI", y="perc_voto", kind="scatter", logx=True)
+    ax.figure.savefig(f"plots/top_{n}_partiesV.png")
+    
+    
+    ax = res.plot(x="SHAPE_AREA", y="perc_voto", kind="scatter", logx=True)
+    ax.figure.savefig(f"plots/top_{n}_partiesA")
+    
+    res["DENSITY"] = res["VOTANTI"]/res["SHAPE_AREA"]
+    ax = res.plot(x="DENSITY", y="perc_voto", kind="scatter", logx=True)
+    ax.figure.savefig(f"plots/top_{n}_partiesD")
 
-max_n = lambda x: x[:n]
-n_past_post = df.groupby(["COMUNE"], group_keys=True, sort=True)[["VOTI_LISTA"]].apply(max_n)
-n_past_post = df[n_past_post]
-print(n_past_post.columns)
-
-n_past_post = n_past_post.groupby("COMUNE")["VOTI_LISTA"].transform(sum).drop_duplicates()
-ax = n_past_post.plot(x="ELETTORI", y="perc_voto", kind="scatter", logx=True)
-ax.figure.savefig("plots/top_{n}_parties.png")
-
+# how are the votes distributed in comuni with different area?
 
 ''' 
 take the votes taken by each candidate in each town and 
@@ -57,8 +72,5 @@ a towny candidates takes 95% of the votes in a comune, but 90% of its votes come
 
 ## indetify towny cnadidates
 with open("data/candidates_clean.csv", "r") as f:
-    cndts = pd.read_csv(f)
-
-with open("data/istat_clean.csv", "r") as f:
-    cmn = pd.read_csv(f)
+    cndts = pd.read_csv(f) 
 
